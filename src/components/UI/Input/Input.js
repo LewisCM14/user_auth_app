4bbc42fec602ffafb1f65e4980ba503d7a8c70ee
LewisCM14 <Lewiscmartin@live.co.uk>
2022-07-01 16:13:52 +0100

    setup login form to use imperativeHandle for validation

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useRef, useImperativeHandle } from "react";
 
-import classes from './Input.module.css';
+import classes from "./Input.module.css";
 
-const Input = (props) => {
-    return (
-        <div
-        className={`${classes.control} ${
-          props.isValid === false ? classes.invalid : ""
-        }`}
-      >
-        <label htmlFor={props.id}>{props.label}</label>
-        <input
-          type={props.type}
-          id={props.id}
-          value={props.value}
-          onChange={props.onChange}
-          onBlur={props.onBlur}
-        />
-      </div>
-    );
-};
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+  const activate = () => {
+    inputRef.current.focus();
+  };
+
+  // refers to the activate function above
+  // requires const to be wrapped in forward ref and ref provided as an argument
+  // imperative handle then exposes the functions you require elsewhere
+  useImperativeHandle(ref, () => {
+    return {
+        focus: activate,
+    };
+  })
+
+  return (
+    <div
+      className={`${classes.control} ${
+        props.isValid === false ? classes.invalid : ""
+      }`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        ref={inputRef}
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
+  );
+});
 
 export default Input;
